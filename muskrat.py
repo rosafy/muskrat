2a05696204ba86421e452a5b0fc06848b9673b04
rosafy <emmadomeisen@gmail.com>
2025-03-05 07:58:47 +0100

    Added browser selection

--- muskrat.py
+++ muskrat.py
@@ -7,12 +7,17 @@ import time
 from datetime import datetime
 import random
 import random_names
-def formfill():
+def formfill(browser):
     cookieok = False
     attempts = 0
     emailformats = ["fn.ln","ln.fn","fnln","fn1ln"]
     emailproviders = ["gmail.com","yahoo.com","outlook.com","hotmail.com","icloud.com"]
-    driver = webdriver.Firefox()
+    if browser == 1:
+        driver = webdriver.Firefox()
+    elif browser == 2:
+        driver = webdriver.Chrome()
+    else:
+        driver = webdriver.Chrome()
     driver.get("https://secured.heritage.org/the-doge-survey/")
     title = driver.title
     elements = [driver.find_element(By.ID,"environmental_protection_agency__5"),driver.find_element(By.ID,"federal_bureau_of_investigation__5"),driver.find_element(By.ID,"department_of_education__5"),driver.find_element(By.ID,"department_of_housing_and_urban_development__5"),driver.find_element(By.ID,"department_of_state__5"),driver.find_element(By.ID,"45_million_for_a_diversity_and_inclusion_scholarship_in_burma__very_supportive"),driver.find_element(By.ID,"3_million_for_girlcentered_climate_action_in_brazil__very_supportive"),driver.find_element(By.ID,"125_million_to_push_critical_race_theory_in_public_health_policy__very_supportive"),driver.find_element(By.ID,"280000_for_diverse_bird_watcher_groups__very_supportive"),driver.find_element(By.ID,"3267000_to_build_a_transgender_health_guide_website_meant_to_increase_access_to_genderaffirming_care_also_known_as_mutilation___very_supportive"),driver.find_element(By.ID,"do_you_believe_the_federal_government_needs_to_be_responsible_with_taxpayer_dollars_and_not_spend_more_money_than_it_takes_in_no"),driver.find_element(By.ID,"do_you_support_significant_cuts_to_the_federal_budget_to_eliminate_budget_deficits_and_pay_down_the_national_debt_no"),driver.find_element(By.ID,"do_you_believe_the_department_of_government_efficiency_doge_will_be_an_effective_tool_for_eliminating_excessive_government_spending__no")]
@@ -64,6 +69,7 @@ def formfill():
     submitbutton.click()
     wait(driver, 15).until_not(EC.title_is(title))
     driver.close()
-
+browser = input("Enter number to select browser to use (1: Firefox, 2: Chrome): ")
+browser = int(browser)
 while True:
-    formfill()
+    formfill(browser)
