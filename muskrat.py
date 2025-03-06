b4a6b36f60a77ee7653b43024480bfa79aa0162f
rosafy <emmadomeisen@gmail.com>
2025-03-06 17:30:51 +0100

    Fixed mistake

--- muskrat.py
+++ muskrat.py
@@ -21,7 +21,7 @@ def formfill(browser):
     driver.get("https://secured.heritage.org/the-doge-survey/")
     title = driver.title
     elements = [driver.find_element(By.ID,"environmental_protection_agency__5"),driver.find_element(By.ID,"federal_bureau_of_investigation__5"),driver.find_element(By.ID,"department_of_education__5"),driver.find_element(By.ID,"department_of_housing_and_urban_development__5"),driver.find_element(By.ID,"department_of_state__5"),driver.find_element(By.ID,"45_million_for_a_diversity_and_inclusion_scholarship_in_burma__very_supportive"),driver.find_element(By.ID,"3_million_for_girlcentered_climate_action_in_brazil__very_supportive"),driver.find_element(By.ID,"125_million_to_push_critical_race_theory_in_public_health_policy__very_supportive"),driver.find_element(By.ID,"280000_for_diverse_bird_watcher_groups__very_supportive"),driver.find_element(By.ID,"3267000_to_build_a_transgender_health_guide_website_meant_to_increase_access_to_genderaffirming_care_also_known_as_mutilation___very_supportive"),driver.find_element(By.ID,"do_you_believe_the_federal_government_needs_to_be_responsible_with_taxpayer_dollars_and_not_spend_more_money_than_it_takes_in_no"),driver.find_element(By.ID,"do_you_support_significant_cuts_to_the_federal_budget_to_eliminate_budget_deficits_and_pay_down_the_national_debt_no"),driver.find_element(By.ID,"do_you_believe_the_department_of_government_efficiency_doge_will_be_an_effective_tool_for_eliminating_excessive_government_spending__no")]
-    starttime = datetime.now().second
+    starttime = time.time()
     while cookieok == False:
         try:
             cookiebutton = driver.find_element(By.CLASS_NAME,"vwo-notification-bar__button")
@@ -31,7 +31,7 @@ def formfill(browser):
             if attempts > 1000:
                 cookieok = True
             else:
-                if datetime.now().second > starttime + 10:
+                if time.time() > starttime + 10:
                     cookieok = True
                 else:
                     continue
@@ -70,7 +70,7 @@ def formfill(browser):
         try:
             submitbutton.click()
         except:
-            if datetime.now().second > starttime + 60:
+            if time.time() > starttime + 60:
                 break
             else:
                 continue
